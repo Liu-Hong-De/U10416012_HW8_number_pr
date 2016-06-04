8b13176b85aadb5e19455936fa481cc3b4c9b2ff
Liu-Hong-De <U10416012@go.utaipei.edu.tw>
2016-06-04 16:45:10 +0800

    create two number to calculate
    create two number to calculate and use BigDecimal to store the two number the user input

---
+++ CalculateDouble.java
@@ -0,0 +1,50 @@
+//U10416012 劉宏德
+
+import java.math.*;
+
+public class CalculateDouble {
+
+	int scale = 20;
+	
+	//create two BigDecimal to store the input number
+	BigDecimal num1;
+	BigDecimal num2;
+
+	public CalculateDouble() {
+	}
+
+	public CalculateDouble(double num1, double num2) {
+		setNum1(num1);
+		setNum2(num2);
+	}
+
+	public CalculateDouble(String num1, String num2) {
+		setNum1(num1);
+		setNum2(num2);
+	}
+	
+	//set num1
+	public void setNum1(double num) {
+		this.num1 = new BigDecimal(num);
+	}
+
+	//set num2
+	public void setNum2(double num) {
+		this.num2 = new BigDecimal(num);
+	}
+
+	//set num1 to String
+	public void setNum1(String num) {
+		this.num1 = new BigDecimal(num);
+	}
+
+	//set num2 to String
+	public void setNum2(String num) {
+		this.num2 = new BigDecimal(num);
+	}
+
+	//set scale
+	public void setScale(int scale) {
+		this.scale = scale;
+	}
+}
